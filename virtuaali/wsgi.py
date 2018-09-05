30cf05cb1f30b49e8b8ae5d115cb88c09b54dbf2
Roosa <roosa.piitulainen@gmail.com>
2018-09-05 15:47:09 +0300

    Update wsgi

--- virtuaali/wsgi.py
+++ virtuaali/wsgi.py
@@ -14,6 +14,6 @@ from django.core.wsgi import get_wsgi_application
 sys.path.append('/home/roosa/illuusion-sim')
 sys.path.append('/home/roosa/illuusion_prod/lib/python3.5/site-packages')
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "virtuaali.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "local_settings")
 
 application = get_wsgi_application()
