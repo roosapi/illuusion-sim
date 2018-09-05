27f5bd63443e591a4d60da7c0421dc1aa1936e1e
Roosa <roosa.piitulainen@gmail.com>
2018-09-05 15:24:23 +0300

    Update wsgi settings

--- virtuaali/wsgi.py
+++ virtuaali/wsgi.py
@@ -7,9 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
+
+sys.path.append(settings.BASE_DIR)
+sys.path.append(os.environ.get('DJANGO_VENV_PATH'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "virtuaali.settings")
 
