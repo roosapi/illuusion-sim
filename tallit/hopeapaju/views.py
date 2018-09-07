638f07ca40e528bbb09298f505ff3e3c61f767d7
Roosa <roosa.piitulainen@gmail.com>
2018-09-07 12:00:49 +0300

    Add pedigree

--- tallit/hopeapaju/views.py
+++ tallit/hopeapaju/views.py
@@ -10,6 +10,44 @@ def index(request):
     template = loader.get_template('hopeapaju/index.html')
     return HttpResponse(template.render({}, request))
 
+def check_horse_address(horse, stable):
+    if horse.stable == stable:
+        horse.address = '../'+horse.address
+    return horse
+
+def get_horse_pedigree(horse, gen_limit=6):
+    maxgen = 0
+    mingen = gen_limit
+    def recurse(h, res, gen, prefix):
+        nonlocal mingen
+        nonlocal maxgen
+        if gen > gen_limit:
+            maxgen = gen_limit
+            return res
+        if not h:
+            if gen < mingen:
+                mingen = gen
+            elif gen > maxgen:
+                maxgen = gen
+            return res
+        else:
+            h = {'prefix': prefix, 'horse': check_horse_address(h, 'hopeapaju')}
+            res.append(h)
+            newgen = gen
+            if not h['horse'].evm:
+                newgen = gen + 1
+            h['rowspan'] = 2**(gen_limit - gen)
+            tempres = recurse(h['horse'].sire, res, newgen, prefix + 'i')
+            finres = recurse(h['horse'].dam, tempres, newgen, prefix + 'e')
+            return finres
+
+    return {
+            'pedigree': recurse(horse.sire, [], 1, 'i') + recurse(horse.dam, [], 1, 'e'),
+            'maxgen': maxgen,
+            'mingen': mingen
+           }
+
+
 def horse(request, slug):
     horse = Horse.objects.select_related('sire', 'dam', 'breeder').get(address=slug)
     #print(slug, horse.name)
@@ -20,22 +58,17 @@ def horse(request, slug):
         offspring = Horse.objects.filter(dam=horse.id)
 
     for foal in offspring:
-        if foal.stable == 'hopeapaju':
-            foal.address = '../'+foal.address
+        foal = check_horse_address(foal, 'hopeapaju')
         
-    sire = horse.sire
-    if sire.stable == 'hopeapaju':
-        sire.address = '../'+sire.address
+    sire = horse.sire#check_horse_address(horse.sire, 'hopeapaju')
 
-    dam = horse.dam
-    if dam.stable == 'hopeapaju':
-        dam.address = '../'+dam.address
-    lines = {}
+    dam = horse.dam#check_horse_address(horse.dam, 'hopeapaju')
+    
+    lines = get_horse_pedigree(horse, 3)
+    print(lines)
     vrl = services.get_vrl_info(horse.vh)
     
     merits = Merit.objects.filter(horse=horse.id)
-    print(merits)
-
     horse.final_description = horse.description.replace(r'\n', '</p><p>')
 
     template = loader.get_template('hopeapaju/horse.html')
@@ -43,7 +76,7 @@ def horse(request, slug):
         'horse': horse,
         'owner': horse.owner,
         'breeder': horse.breeder,
-        'lineage': {'sire':sire, 'dam':horse.dam},
+        'lineage': {'sire':sire, 'dam':dam, 'lines':lines},
         'vrl_info': vrl,
         'offspring': offspring,
         'merits': merits,
